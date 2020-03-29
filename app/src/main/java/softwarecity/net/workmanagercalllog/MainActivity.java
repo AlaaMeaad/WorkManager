629c9a1f4b10af751213b858b9c16f33f67b58aa
AlaaMeaad <3la2.me3ad@gmail.com>
2020-03-29 14:29:43 +0200

    button

--- app/src/main/java/softwarecity/net/workmanagercalllog/MainActivity.java
+++ app/src/main/java/softwarecity/net/workmanagercalllog/MainActivity.java
@@ -19,6 +19,8 @@ import android.os.Bundle;
 import android.provider.CallLog;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.text.DateFormat;
@@ -35,9 +37,20 @@ public class MainActivity extends AppCompatActivity {
     static MainActivity mainActivity;
     public static StringBuffer sb;
     DataManagerImpl dataManager;
+    Button bnt_data;
+    WorkMang workMang;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bnt_data = (Button) findViewById(R.id.bnt_data);
+        bnt_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            workMang.allFilds();
+
+            }
+        });
+
         Data data = new Data.Builder()
                 .putInt("number" , 10)
                 .build();
@@ -63,19 +76,18 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSION_REQUEST_READ_PHONE_STATE: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "Permission granted: " + PERMISSION_REQUEST_READ_PHONE_STATE, Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(this, "Permission NOT granted: " + PERMISSION_REQUEST_READ_PHONE_STATE, Toast.LENGTH_SHORT).show();
-                }
-
-                return;
-            }
-        }
-    }
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+//        switch (requestCode) {
+//            case PERMISSION_REQUEST_READ_PHONE_STATE: {
+//                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    Toast.makeText(this, "Permission granted: " + PERMISSION_REQUEST_READ_PHONE_STATE, Toast.LENGTH_SHORT).show();
+//                } else {
+//                    Toast.makeText(this, "Permission NOT granted: " + PERMISSION_REQUEST_READ_PHONE_STATE, Toast.LENGTH_SHORT).show();
+//                }
+//
+//                return;
+//            }
+//        }
+//    }
 }
